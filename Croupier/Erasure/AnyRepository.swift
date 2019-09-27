6810ddd7c0f8cab50d289175c99fb9b951393107
mrdanb <hellodanb@gmail.com>
2019-09-27 14:48:19 +0100

    Reintegrate the AnyRepository type

--- Croupier/Erasure/AnyRepository.swift
+++ Croupier/Erasure/AnyRepository.swift
@@ -1,32 +1,32 @@
 import Foundation
 
-/*public struct AnyRepository<ModelType>: Repository {
+public struct AnyRepository<Response,Entity>: Repository where Response: Serializable {
 
-    private let _get: (String, @escaping (Result<ModelType,Error>) -> Void) -> Void
-    private let _getAll: (@escaping (Result<[ModelType],Error>) -> Void) -> Void
-    private let _sync: (@escaping (Result<Bool,Error>) -> Void) -> Void
-    private let _delete: (ModelType, @escaping (Result<ModelType,Error>) -> Void) -> Void
-    public init<Repo>(_ repository: Repo) where Repo: Repository, Repo.ModelType == ModelType {
+    private let _get: (String, @escaping (Result<Entity,Error>) -> Void) -> Void
+    private let _getAll: (@escaping (Result<[Entity],Error>) -> Void) -> Void
+    private let _sync: (String, @escaping (Result<Changes<Entity>,Error>) -> Void) -> Void
+    private let _delete: (Entity, @escaping (Result<Entity,Error>) -> Void) -> Void
+    public init<Repo>(_ repository: Repo) where Repo: Repository, Repo.Entity == Entity {
         _get = repository.get
         _getAll = repository.getAll
         _sync = repository.sync
         _delete = repository.delete
     }
 
-    public func get(forKey key: String, completion: @escaping (Result<ModelType, Error>) -> Void) {
+    public func get(forKey key: String, completion: @escaping (Result<Entity, Error>) -> Void) {
         _get(key, completion)
     }
 
-    public func getAll(completion: @escaping (Result<[ModelType], Error>) -> Void) {
+    public func getAll(completion: @escaping (Result<[Entity], Error>) -> Void) {
         _getAll(completion)
     }
 
-    public func sync(completion: @escaping (Result<Bool, Error>) -> Void) {
-        _sync(completion)
+    public func sync(key: String, completion: @escaping (Result<Changes<Entity>, Error>) -> Void) {
+        _sync(key, completion)
     }
 
-    public func delete(item: ModelType, completion: @escaping (Result<ModelType, Error>) -> Void) {
+    public func delete(item: Entity, completion: @escaping (Result<Entity, Error>) -> Void) {
         _delete(item, completion)
     }
 }
-*/
+
