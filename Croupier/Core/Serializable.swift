8c19d12b04fd7f1133fffb65da18a8fb51aca291
mrdanb <hellodanb@gmail.com>
2019-09-27 18:09:59 +0100

    Adds some default implementations of the serialisation methods

--- Croupier/Core/Serializable.swift
+++ Croupier/Core/Serializable.swift
@@ -1,7 +1,26 @@
 import Foundation
+import CoreData
 
 public protocol Serializable {
     associatedtype Serialized
     associatedtype Context
     func serialize(context: Context) -> [Serialized]
 }
+
+public extension Serializable {
+    func serialize(context: EmptyContext) -> [Self] {
+        return [self]
+    }
+}
+
+public extension Serializable where Self: NSManagedObject {
+    func serialize(context: NSManagedObjectContext) -> [Self] {
+        let name = self.entity.name ?? String(describing: self)
+        guard let object =  NSEntityDescription.insertNewObject(forEntityName: name, into: context) as? Self else {
+            return []
+        }
+        return [object]
+    }
+}
+
+public protocol EmptyContext {}
