822fe0c097b71cd08932ef881e4ab7797c71fb34
danbennett <hellodanb@gmail.com>
2019-09-29 18:06:24 +0100

    Adds another serialise method that does not except a Context

--- Croupier/Core/Serializable.swift
+++ Croupier/Core/Serializable.swift
@@ -5,22 +5,15 @@ public protocol Serializable {
     associatedtype Serialized
     associatedtype Context
     func serialize(context: Context) -> [Serialized]
+    func serialize() -> [Serialized]
 }
 
 public extension Serializable {
-    func serialize(context: EmptyContext) -> [Self] {
+    func serialize() -> [Self] {
+         return [self]
+    }
+
+    func serialize(context: Any) -> [Self] {
         return [self]
     }
 }
-
-public extension Serializable where Self: NSManagedObject {
-    func serialize(context: NSManagedObjectContext) -> [Self] {
-        let name = self.entity.name ?? String(describing: self)
-        guard let object =  NSEntityDescription.insertNewObject(forEntityName: name, into: context) as? Self else {
-            return []
-        }
-        return [object]
-    }
-}
-
-public protocol EmptyContext {}
